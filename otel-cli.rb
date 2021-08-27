bef4280f1fa1f95694c15591083a86976a447619
Amy Tobey <atobey@equinix.com>
2021-08-27 14:30:41 -0700

    release v0.0.8
    goreleaser token failed because branch protection rules, I need to work
    with the org admins to see if we can keep that off for automated
    releases on this repo

--- otel-cli.rb
+++ otel-cli.rb
@@ -5,29 +5,29 @@
 class OtelCli < Formula
   desc "OpenTelemetry command-line tool for sending events from shell scripts & similar environments"
   homepage "https://github.com/equinix-labs/otel-cli"
-  version "0.0.7"
+  version "0.0.8"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.7/otel-cli-0.0.7-Darwin-x86_64.tar.gz"
-      sha256 "0ffcf47dca08f12b7126a009a93de4a625e00e33adfc76f02119df476180c614"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.8/otel-cli-0.0.8-Darwin-x86_64.tar.gz"
+      sha256 "c2037ad955d0ca35366bbc4c36aa3d60ff2cd5b7c3aecc762b77d5c0643ee78c"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.7/otel-cli-0.0.7-Darwin-arm64.tar.gz"
-      sha256 "2d287594d63677acfc27d6398e7fb6185495ce4784c11f73fc482844aa884513"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.8/otel-cli-0.0.8-Darwin-arm64.tar.gz"
+      sha256 "0639cec14d618d6ac488e8fb65dd1f164f992aa4a7aff4e14806c5c29adf64fd"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.7/otel-cli-0.0.7-Linux-x86_64.tar.gz"
-      sha256 "d31358e635f4502e2770053b0f8859e19aead4bb6815c73718f52738f44aca4b"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.8/otel-cli-0.0.8-Linux-x86_64.tar.gz"
+      sha256 "b72ee0a68668ad6b9cd9eb300c2f87e0e2d90d906e47c52f30d16d20ddb4c721"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.7/otel-cli-0.0.7-Linux-arm64.tar.gz"
-      sha256 "5a505e763cb3cf07bbbe6ca3ce3ac7eaad10a9f5835202ff4ff78aaac2275cd7"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.8/otel-cli-0.0.8-Linux-arm64.tar.gz"
+      sha256 "ad1a11737d7816ec8097995818aada34e171b5295086acf1c53d3441f553d2fd"
     end
   end
 
