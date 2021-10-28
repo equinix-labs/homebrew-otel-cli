1f345e507f135e33c118580dc038e3ad148cf2ac
tobert <atobey@equinix.com>
2021-10-28 11:28:39 -0700

    Brew formula update for otel-cli version v0.0.19

--- otel-cli.rb
+++ otel-cli.rb
@@ -5,29 +5,29 @@
 class OtelCli < Formula
   desc "OpenTelemetry command-line tool for sending events from shell scripts & similar environments"
   homepage "https://github.com/equinix-labs/otel-cli"
-  version "0.0.18"
+  version "0.0.19"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli-0.0.18-Darwin-x86_64.tar.gz"
-      sha256 "7a3a046cc20dcaf7d2353234e548f762b303747dc62f12fa09634c21a880d7e9"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Darwin-x86_64.tar.gz"
+      sha256 "531eae4177d863f07210c2608c295b89c4fdc651f6e8fa667c8a6b9c530afeff"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli-0.0.18-Darwin-arm64.tar.gz"
-      sha256 "7876b2c2b8b56ef508e36f6843899ce79af59e92ffba9776eb086f3da21f62a6"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Darwin-arm64.tar.gz"
+      sha256 "abad5d4b541663e4c5abcda18c02a89d51a7f39501edd4026ab25989c2621f0e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli-0.0.18-Linux-x86_64.tar.gz"
-      sha256 "b9623ef0afa5f51d09c53d475916835cd3bb1f0beb68357558c3c4220d953a33"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Linux-x86_64.tar.gz"
+      sha256 "aaedd5b0b06abeee9a0e4fb605b3f48fc13e2d055904d78f3923a646768f8c2d"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli-0.0.18-Linux-arm64.tar.gz"
-      sha256 "4fc36d606421116710c509367068f6957037be02a5ec0eb26b30f01832aaa4cf"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Linux-arm64.tar.gz"
+      sha256 "10f8e85353e7b7f5fb2293518382792b567bd4b729672f445282597185ca1026"
     end
   end
 
