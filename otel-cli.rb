eb032935009d2ad4728130626bcafbaff142c57b
Amy Tobey <atobey@equinix.com>
2022-02-24 11:05:21 -0800

    add automatically generated code from goreleaser

--- otel-cli.rb
+++ otel-cli.rb
@@ -5,32 +5,44 @@
 class OtelCli < Formula
   desc "OpenTelemetry command-line tool for sending events from shell scripts & similar environments"
   homepage "https://github.com/equinix-labs/otel-cli"
-  version "0.0.19"
+  version "0.0.20"
   license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Darwin-x86_64.tar.gz"
-      sha256 "531eae4177d863f07210c2608c295b89c4fdc651f6e8fa667c8a6b9c530afeff"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Darwin-arm64.tar.gz"
-      sha256 "abad5d4b541663e4c5abcda18c02a89d51a7f39501edd4026ab25989c2621f0e"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.20/otel-cli-0.0.20-Darwin-arm64.tar.gz"
+      sha256 "369cf357950c2617b551b1764702ddf985431ead4ed3262f41bc8f888d83e64a"
+
+      def install
+        bin.install "otel-cli"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.20/otel-cli-0.0.20-Darwin-x86_64.tar.gz"
+      sha256 "7efbef2f5c0889f17c1c50539ca0f60dbbcf5855aad0fc28829de16268eb11f6"
+
+      def install
+        bin.install "otel-cli"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Linux-x86_64.tar.gz"
-      sha256 "aaedd5b0b06abeee9a0e4fb605b3f48fc13e2d055904d78f3923a646768f8c2d"
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.20/otel-cli-0.0.20-Linux-x86_64.tar.gz"
+      sha256 "3e4132d02a3c2a011f11d113f7f223f7008f55c785c347dc9009d5e79da1e210"
+
+      def install
+        bin.install "otel-cli"
+      end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.19/otel-cli-0.0.19-Linux-arm64.tar.gz"
-      sha256 "10f8e85353e7b7f5fb2293518382792b567bd4b729672f445282597185ca1026"
-    end
-  end
+      url "https://github.com/equinix-labs/otel-cli/releases/download/v0.0.20/otel-cli-0.0.20-Linux-arm64.tar.gz"
+      sha256 "8e9ec92605fbddc00a7f4a8c60df4296e094694a94fee3fdba6f24d3a89e3e48"
 
-  def install
-    bin.install "otel-cli"
+      def install
+        bin.install "otel-cli"
+      end
+    end
   end
 end
